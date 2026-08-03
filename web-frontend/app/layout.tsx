273031ba65eb94f546ce67e50181356cba207984
ganeshvgs <197109023+ganeshvgs@users.noreply.github.com>
2026-08-03 21:36:14 +0530

    updating seo images

--- web-frontend/app/layout.tsx
+++ web-frontend/app/layout.tsx
@@ -22,6 +22,23 @@ const siteUrl =
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
 
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/apple-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/favicon.ico",
+  },
+
   title: {
     default:
       "Byndoor (Baindur), Karnataka – Places to Visit, Beaches, Waterfalls & Travel Guide",
