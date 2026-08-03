f990a848463d1725bda139cadc81b951366fc8a6
ganeshvgs <197109023+ganeshvgs@users.noreply.github.com>
2026-08-03 14:23:45 +0530

    ejncw jnvd

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,8 @@ app.use(
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "https://byndoor.kundapura.in/",
+  "https://www.byndoor.kundapura.in",
   "https://nammabyndoor.vercel.app",
 ];
 
